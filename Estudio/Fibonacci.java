a159a2c99668c1a097faf0e528b0c0ac87f83dc1
Geovanny Alcon <151603150+g4usSi@users.noreply.github.com>
2024-02-06 17:31:47 -0600

    Add files via upload

---
+++ Estudio/Fibonacci.java
@@ -0,0 +1,32 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+    System.out.println("El siguiente programa muestra los valores de la serie Fibonacci");
+        int i = 5;
+        System.out.println("val i "+(i--));
+        System.out.println("val i "+(i));
+        System.out.println("parte 2 ");
+        i = 5;
+        System.out.println("val i "+(--i));
+        System.out.println("val i "+(i));
+
+        int pos = 14;
+        Fibonacci tmp = new Fibonacci();
+        int res = tmp.resolveF( pos);
+        System.out.println("pos: "+pos + " es: "+res);
+    }
+
+    private int resolveF(int res) {
+        System.out.print(" 0 1");
+        return resolveF(0, 1, res);
+    }
+
+    private int resolveF(int ant, int act, int res){
+        if (res!=0){
+            System.out.print(" "+(act+ant));
+            return resolveF(act, (act+ant), --res);
+        }
+
+        return act;
+    }
+
+}
---
+++ Estudio/Programa1.java
@@ -0,0 +1,22 @@
+package Estudio;
+
+public class Programa1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo :3");
+
+        //creando una matriz de dos dimensiones, para hacer calculos donde debe escribir cada caracter
+        for (int i=0; i<6; i++) {
+            for (int j=0; j<7; j++) {
+                if ((i==0 && j%3 != 0)||(i==1 && j%3==0)||(i-j == 2 || i+j == 8 )) {
+                System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.print("  ");
+            System.out.println();
+        }
+        
+    }
+    //*Version 1.0 creo que se puede simplificar mas con una matriz en int [][] :) */
+}
